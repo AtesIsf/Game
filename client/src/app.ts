af440241baa3f9dab50b18d3fdc915fcd47b5051
Ates <88388753+AtesIsf@users.noreply.github.com>
2024-11-18 17:54:20 -0500

    enabled image smoothing

--- client/src/app.ts
+++ client/src/app.ts
@@ -5,6 +5,9 @@ canvas.width = window.outerWidth;
 canvas.height = window.outerHeight;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
+ctx.imageSmoothingEnabled = true;
+ctx.imageSmoothingQuality = "high";1
+
 Helpers.drawGrid(ctx, 3, 3);
 Helpers.drawEntity(ctx, "infantry.png", 2, 1);
 Helpers.drawEntity(ctx, "tower.png", 1, 0);
