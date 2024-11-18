ee3f580719e6fa12607e903c2081bfc0284802eb
Ates <88388753+AtesIsf@users.noreply.github.com>
2024-11-18 12:20:54 -0500

    Delete server/target directory

--- server/target/debug/deps/log-102616d67369fd41.d
+++
@@ -1,8 +0,0 @@
-/home/ates-isf/Programming/Game/server/target/debug/deps/liblog-102616d67369fd41.rmeta: /home/ates-isf/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/lib.rs /home/ates-isf/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/macros.rs /home/ates-isf/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/serde.rs /home/ates-isf/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/__private_api.rs
-
-/home/ates-isf/Programming/Game/server/target/debug/deps/log-102616d67369fd41.d: /home/ates-isf/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/lib.rs /home/ates-isf/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/macros.rs /home/ates-isf/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/serde.rs /home/ates-isf/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/__private_api.rs
-
-/home/ates-isf/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/lib.rs:
-/home/ates-isf/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/macros.rs:
-/home/ates-isf/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/serde.rs:
-/home/ates-isf/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/__private_api.rs:
