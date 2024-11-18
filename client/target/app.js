357a18fc788c2a38ff3d4434d51abe191f933f3d
Ates <88388753+AtesIsf@users.noreply.github.com>
2024-11-18 12:22:34 -0500

    Delete client/target directory

--- client/target/app.js
+++
@@ -1,6 +0,0 @@
-import { Helpers } from "./helpers.js";
-const canvas = document.getElementById("game");
-canvas.width = window.outerWidth;
-canvas.height = window.outerHeight;
-const ctx = canvas.getContext("2d");
-Helpers.drawGrid(ctx, 80, 80, 3, 3);
